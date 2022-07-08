3aa0ff22e12ad16b330f7d4504ff000eef942734
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-08 22:12:25 +0530

    Calculate wages till a condition of total working hours

--- src/Com/BridgeLabz/EmployeeWage.java
+++ src/Com/BridgeLabz/EmployeeWage.java
@@ -2,49 +2,48 @@ package Com.BridgeLabz;
 
  public class EmployeeWage {
 
-    static final int IS_PRESENT=1;
+    static final int IS_FULL_TIME=1;
     static final int IS_PART_TIME=2;
     static final int WAGE_PER_HOUR=20;
     static final int FULL_DAY_HOUR=8;
     static final int PART_TIME_HOUR=4;
     static final int WORKING_DAY_PER_MONTH=20;
+    static final int TOTAL_WORK_HOURS=100;
 
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-
+        int totalHours=0;
+        int day=0;
         int dailyWage=0;
-        int monthlyWage=0;
+        int totalWage=0;
 
-        for(int day=1; day<=WORKING_DAY_PER_MONTH; day++) {
+        while((totalHours<TOTAL_WORK_HOURS) && (day<WORKING_DAY_PER_MONTH)) {
             int attendence = (int) (Math.floor(Math.random() * 10)) % 3;
 
 
             switch (attendence) {
 
-                case IS_PRESENT:
-                    // System.out.println("Employee is Working Full Time");
+                case IS_FULL_TIME:
                     dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    System.out.println("Employee daily wage is :" + dailyWage);
-                    //int monthlyWage = dailyWage * WORKING_DAY_PER_MONTH;
-                    //System.out.println("Employee monthly wage is : "+ monthlyWage);
+                    totalHours += FULL_DAY_HOUR;
                     break;
 
                 case IS_PART_TIME:
-                    // System.out.println("Employee is working part time");
                     dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    System.out.println("Employee daily wage is: " + dailyWage);
-                    //monthlyWage = dailyWage * WORKING_DAY_PER_MONTH;
-                    //System.out.println("Employee monthly wage is : "+ monthlyWage);
+                    totalHours += PART_TIME_HOUR;
                     break;
 
                 default:
-                    System.out.println("Employee is absent");
                     break;
             }
-            monthlyWage += dailyWage;
+            totalWage += dailyWage;
+            day++;
         }
-        System.out.println("monthlyWage :"+monthlyWage);
+        System.out.println("Total work hours is : "+totalHours);
+        System.out.println("Total work days are: " +day);
+        System.out.println("Total wage is: " +totalWage);
+
 
     }
  }
