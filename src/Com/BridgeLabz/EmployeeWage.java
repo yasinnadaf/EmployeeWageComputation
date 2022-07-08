3719c470f7cceca6d795fd4f642fe26806c1b8e4
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-08 17:54:55 +0530

    Updated: Daily Employee Wage Calculated

--- src/Com/BridgeLabz/EmployeeWage.java
+++ src/Com/BridgeLabz/EmployeeWage.java
@@ -1,18 +1,27 @@
 package Com.BridgeLabz;
 
-public class EmployeeWage {
+ public class EmployeeWage {
+
+    static final int EMPLOYEE_PRESENT=1;
+    static final int WAGE_PER_HOUR=20;
+    static final int FULL_DAY_HOUR=8;
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
+        int dailyWage=0;
         int attendence= (int) (Math.floor(Math.random()*10))%2;
 
-        if(attendence==1) {
+        if(attendence==EMPLOYEE_PRESENT) {
             System.out.println("Employee is present");
+              dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
         }
 
         else {
             System.out.println("Employee is absent");
         }
+        System.out.println("Daily wage: "+dailyWage);
 
 
     }
