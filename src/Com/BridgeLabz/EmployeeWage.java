b2b02455fcde01cca996522602151e39c68874dd
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-08 17:30:44 +0530

    Added: Employee_Attendence

--- src/Com/BridgeLabz/EmployeeWage.java
+++ src/Com/BridgeLabz/EmployeeWage.java
@@ -1,12 +1,14 @@
 package Com.BridgeLabz;
 
 public class EmployeeWage {
+
+    static final int EMOPlOYEE_PRESENT=1;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
 
         int attendence= (int) (Math.floor(Math.random()*10))%2;
 
-        if(attendence==1) {
+        if(attendence==EMOPlOYEE_PRESENT) {
             System.out.println("Employee is present");
         }
 
